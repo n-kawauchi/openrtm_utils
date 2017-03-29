81656b5411cb96ffc701e223daa44ae87d58e1b4
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2017-03-29 14:40:01 +0900

    1439

--- SettingRTSystem/SettingRTSystem/rtcdControlPy/idlcompile.bat
+++ SettingRTSystem/SettingRTSystem/rtcdControlPy/idlcompile.bat
@@ -1,3 +1,13 @@
 cd /d %~dp0
 
-omniidl.exe -bpython rtcControl.idl
+
+echo off
+setlocal
+for %%I in (python.exe) do if exist %%~$path:I set f=%%~$path:I
+if exist %f% (
+  %f:python.exe=%omniidl.exe -bpython idl/rtcControl.idl
+) else (
+  echo "python.exe" can not be found.
+  echo Please modify PATH environmental variable for python command.
+)
+endlocal
