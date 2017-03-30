8af72890b6c50a90460947fc6f944afb0129089e
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2017-03-30 13:48:55 +0900

    1348

--- SettingRTSystem/SettingRTSystem/rtcConfSet/setup.py
+++ SettingRTSystem/SettingRTSystem/rtcConfSet/setup.py
@@ -11,6 +11,13 @@ if "PACKAGE_DIRECTORIY" in os.environ:
 	package_dir = os.environ["PACKAGE_DIRECTORIY"].replace("\"","")
 	sys.path.append(package_dir)
 
+
+if "SettingRTSystemPath" in os.environ:
+	sys.path.append(os.environ["SettingRTSystemPath"])
+	sys.path.append(os.path.join(os.environ["SettingRTSystemPath"],"RTCD_IDL"))
+	sys.path.append(os.path.join(os.environ["SettingRTSystemPath"],"rtcConfSet"))
+
+
 NAME = 'rtcConfSet'
 LICENSE = 'GPL'
 AUTHOR = ''
@@ -25,7 +32,7 @@ if sys.platform == 'win32':
     PY2EXE_OPTIONS = {'compressed': 1,
                       'optimize': 2,
                       'bundle_files': 3,
-                      'includes': ["rtcprofile_plugin","lxml._elementpath","commands","ActArray_idl","AIO_idl","Bumper_idl","Camera_idl","DIO_idl","Fiducial_idl","GPS_idl","Gripper_idl","IMU_idl","INS_idl","Limb_idl","Map_idl","Multicamera_idl","PanTilt_idl","Paths_idl","PointCloud_idl","Position_idl","Ranger_idl","RFID_idl"],}
+                      'includes': ["rtcprofile_plugin","rtcControl_idl","rtcconf_idl","lxml._elementpath","commands","ActArray_idl","AIO_idl","Bumper_idl","Camera_idl","DIO_idl","Fiducial_idl","GPS_idl","Gripper_idl","IMU_idl","INS_idl","Limb_idl","Map_idl","Multicamera_idl","PanTilt_idl","Paths_idl","PointCloud_idl","Position_idl","Ranger_idl","RFID_idl"],}
     
     INNOSETUP_OPTIONS = {'inno_script': innosetup.DEFAULT_ISS,
                          'bundle_vcr': True,
